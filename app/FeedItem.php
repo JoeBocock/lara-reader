f63575a7e79370397e2d21c379a2a4d894dc561d
Joe Bocock <joebocock@gmail.com>
2020-10-28 17:51:40 +0000

    Stop protection of columns as we're explicit in building the insert array.

--- app/FeedItem.php
+++ app/FeedItem.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class FeedItem extends Model
 {
+    protected $guarded = [];
+
     public function feed()
     {
         return $this->belongsTo(Feed::class);
