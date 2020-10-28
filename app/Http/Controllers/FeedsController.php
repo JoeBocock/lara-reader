e60240703cb916bfadac68f1804a0b62ed612258
Joe Bocock <joebocock@gmail.com>
2020-10-28 17:53:16 +0000

    POST::Store a feed

--- app/Http/Controllers/FeedsController.php
+++ app/Http/Controllers/FeedsController.php
@@ -2,15 +2,43 @@
 
 namespace App\Http\Controllers;
 
+use App\Feed;
+use App\FeedItem;
+use Illuminate\Http\Response;
 use App\Http\Common\RSSHandler;
+use Illuminate\Http\JsonResponse;
+use App\Http\Common\Helpers\Arrays;
 use App\Http\Requests\StoreFeedRequest;
 
 class FeedsController extends Controller
 {
-    public function store(StoreFeedRequest $request, RSSHandler $handler)
+    /**
+     * Store a requested RSS feed.
+     *
+     * @param StoreFeedRequest $request
+     * @param RSSHandler $handler
+     * @return JsonResponse
+     */
+    public function store(StoreFeedRequest $request, RSSHandler $handler): JsonResponse
     {
-        $feedItems = $handler->convertToDBFormat($request->url);
+        // Convert the passed RSS URL into usable data
+        $handlerResponse = $handler->convertToDBFormat($request->url);
 
-        return response()->json($feedItems);
+        // If the conversion was not successful, return the error bag with HTTP code.
+        if (isset($handlerResponse['success']) && !$handlerResponse['success']) {
+            return new JsonResponse($handlerResponse, Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        // Store the Feed
+        $feed = Feed::create($handlerResponse['feed']);
+
+        // Key the feed items with their parents ID
+        $handlerResponse['items'] = Arrays::addKeyToChildren($handlerResponse['items'], 'feed_id', $feed->id);
+
+        // Bulk insert the data
+        FeedItem::insert($handlerResponse['items']);
+
+        // Response to request with created HTTP code and return core object
+        return new JsonResponse($feed, Response::HTTP_CREATED);
     }
 }
